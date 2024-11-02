2b504f2c7f7ecb97adfbdcc68a4e64b9f6e26615
Anastasia <ponomarenko0213@mail.ru>
2024-11-02 14:13:15 +0300

    commit2

---
+++ src/test/java/L5/SellerManagerTest.java
@@ -0,0 +1,56 @@
+package L5;
+
+import org.example.L5.exception.SellerIsBusyException;
+import org.example.L5.model.Specialize;
+import org.example.L5.model.buyer.Buyer;
+import org.example.L5.model.seller.Consultant;
+import org.example.L5.model.seller.Seller;
+import org.example.L5.service.SellerManager;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SellerManagerTest {
+
+    private static final Seller consultant = new Consultant(Specialize.CONSULTANT);
+    private static final Seller merchandiser = new Consultant(Specialize.MERCHANDISER);
+    private static final LocalDate birthday = LocalDate.of(2000, 8, 10);
+    private static final LocalDate date = LocalDate.of(2024, 11, 10);
+    private static final Buyer buyer = new Buyer("FIO", Specialize.CASHIER, birthday, date);
+    private static final Buyer buyer2 = new Buyer("FIO", Specialize.MERCHANDISER, birthday, date);
+    private static final Buyer buyer3 = new Buyer("FIO", Specialize.CONSULTANT, birthday, date);
+    private static final SellerManager sellerManager = new SellerManager();
+    List<Seller> sellers = List.of(consultant, merchandiser);
+
+
+    @AfterEach
+    void clearShelves() {
+        sellers.forEach(Seller::clearSeller);
+    }
+
+    @Test
+    @DisplayName("Проверим, у всех консультантов есть покупатели")
+    void allSellersHaveCorrectBuyers() {
+        assertDoesNotThrow(() ->
+                sellerManager.addBuyerToSeller(consultant, buyer3));
+        assertDoesNotThrow(() ->
+                sellerManager.addBuyerToSeller(merchandiser, buyer2));
+        for (Seller seller : sellers) {
+            for (Buyer buyer : seller.getBuyers()) {
+                Assertions.assertEquals(seller.getSpecializeType(), buyer.getPreferences());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Ошибка добавления покупателя к консультанту")
+    void sellerDoesNotExistsForBuyer() {
+        assertThrows(SellerIsBusyException.class,
+                () -> sellerManager.addBuyerToSeller(consultant, buyer));
+    }
+
+}
+
